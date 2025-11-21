04dcb64f99de02f88b92d70daa4cb2f86bce15b1
soham pankaj mamdyal <75490885+sohammamdyal@users.noreply.github.com>
2025-11-21 18:11:44 +0530

    added dotenv deps

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-
+require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const API_BASE = "https://fakestoreapi.com";  // FIXED ✔
