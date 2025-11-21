bbfc296f56ee10cefb847a676154d4470a8d6674
soham pankaj mamdyal <75490885+sohammamdyal@users.noreply.github.com>
2025-11-21 18:05:17 +0530

    initial commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 const API_BASE = "https://fakestoreapi.com";  // FIXED ✔
 
 app.use(cors());
